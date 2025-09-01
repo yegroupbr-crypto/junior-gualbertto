6dd03a7faf0faf86faf8c078043443bc405cc15d
Ye Group <yegroupbr@gmail.com>
2025-09-01 19:51:38 +0000

    Imagem para adicionar ao perfil aqui em anexo

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
